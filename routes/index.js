a92c5c12f4c56f0b89d4209aec6523184e2e0128
James Leddy <jwleddy@gmail.com>
2016-05-28 14:57:38 -0700

    just startin with moby

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var moby = require('moby');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', searchResult: moby.search('mad') });
 });
 
 module.exports = router;
